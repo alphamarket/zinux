e4dbe6bb10cf2b845cb961efabdd8fa455757ec8
dariushha <b.g.dariush@gmail.com>
2013-05-23 19:21:33 +0430

    Application.php is compatible with ActiveRecord injection. Operation taking care of in Application class in order to make ActiveRecord operational:
    + Config ActiveRecord connections string
    + Loading db data from conf file
    + Create dynamic connection string based on config file's data
    + Dynamic db selection base on RUNNING_ENV
    + Loading relative Model\DB path according to requested URI
    + Checking database connections after configurations [ if it didn't succeed a notifier will be hit ]
    + Some other changes to make project compatible with ActiveRecords

--- Application/Application.php
+++ Application/Application.php
@@ -8,6 +8,8 @@ require_once 'Tools/Debug.php';
 require_once 'Exceptions/include.all.inc';
 require_once 'Controller/BaseController.php';
 require_once 'Tools/Config.php';
+require_once 'DB/php-activerecord/ActiveRecord.php';
+use \ActiveRecord;
 
 class Application extends \iMVC\BaseMVC 
 {
@@ -29,7 +31,14 @@ class Application extends \iMVC\BaseMVC
             $this->Startup ("");
         }
         $r = new \iMVC\Routing\Router();
-        $r->Run(new \iMVC\Routing\Request());
+        
+        $req = new \iMVC\Routing\Request();
+        
+        $req->Initiate();
+        
+        $this->LoadActiveRecord($req);
+       
+        $r->Run($req);
     }
     /**
      * Startup and making application's ready with passed configuration
@@ -68,5 +77,61 @@ __END:
     public function Shutdown()
     {
         $this->Dispose();
+    }
+    
+    protected function LoadActiveRecord(\iMVC\Routing\Request $request)
+    { 
+        if(!isset($GLOBALS['CONFIGS']['db']))
+        {
+            trigger_error("In configuration db setting did not found, ActiveRecord is not loaded ....");
+            return;
+        }
+        if(isset($GLOBALS['CONFIGS']['db']['suppress']) && $GLOBALS['CONFIGS']['db']['suppress'])
+        {
+            trigger_error("Suppressed database active record loader.");
+            return;
+        }
+        if(!isset($GLOBALS["CONFIGS"]['db']['type']))
+            $GLOBALS["CONFIGS"]['db']['type'] = "mysql";
+
+        $db_type = $GLOBALS["CONFIGS"]['db']['type'];
+        $username = $GLOBALS['CONFIGS']['db']['username'];
+        $password = $GLOBALS['CONFIGS']['db']['password'];
+        $host = $GLOBALS['CONFIGS']['db']['host'];
+        $db_name = $GLOBALS['CONFIGS']['db']['name'];
+
+        # define a dynamic connection string according to incomming request
+        $connections = array(
+            RUNNING_ENV => "$db_type://{$username}:{$password}@{$host}/{$db_name}",
+        );
+
+        # must issue a "use" statement in your closure if passing variables
+        \ActiveRecord\Config::initialize(function($cfg) use ($connections, $request)
+        {
+            $db_m_path = MODULE_PATH."{$request->module}/Models/DB";
+
+            if(!file_exists($db_m_path))
+            {
+                trigger_error("Database model's directory didn't found at '$db_m_path'");
+            }
+
+            \ActiveRecord\Config::instance()->set_model_directory($db_m_path);
+
+            $cfg->set_connections($connections);
+            
+            $cfg->set_default_connection(RUNNING_ENV);
+            
+        });
+        # test database connection
+        try
+        {
+             ActiveRecord\Connection::instance($connections[RUNNING_ENV])->query("SHOW TABLES;");
+        }
+        catch (\Exception $pdoe)
+        {
+            trigger_error("Could not stablish a connection with database : '{$connections[RUNNING_ENV]}'.");
+            echo "<br />Message: <br />";
+            echo \iMVC\Tools\Debug::_var($pdoe->getMessage());
+        }
     }
 }
