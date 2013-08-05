dd4f976c0ef7e8ad6f1f0437269ec1e81bfd70ca
dariush <b.g.dariush@gmail.com>
2013-08-06 02:31:00 +0430

    + SecurityException.php + iMVC/Security/include.all.inc + InvalidCookireException.php + SecureCookire.php

---
+++ Exceptions/SecurityException.php
@@ -0,0 +1,19 @@
+<?php
+namespace iMVC\Exceptions;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of SecurityException
+ *
+ * @author dariush
+ */
+require 'AppException.php';
+class SecurityException extends \iMVC\Exceptions\AppException
+{
+    //put your code here
+}
+
+?>
---
+++ Security/SecureCookie.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace iMVC\Security;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Set secure cookie
+ *
+ * @author dariush
+ */
+require_once 'Hash.php';
+class SecureCookie 
+{
+    public function __get($name)
+    {
+        if(!self::Contains($name))
+            return false;
+        return $_COOKIE[$name];
+    }
+    
+    /**
+     * @param string $name
+     * @param array $value_expire expected format { array('value'=>$value, 'expire'=>$expire) }
+     */
+    public function __set($name, array $value_expire) 
+    {
+        self::Set($name, $value_expire['value'], $value_expire['expire']);
+    }
+
+    /**
+     * Set a secure cookie
+     * @param string $name
+     * @param string $value
+     * @param integer $expire_from_now
+     */
+    public static function Set($name, $value, $expire_from_now)
+    {
+        setcookie("$name", $value, time()+$expire_from_now); 
+        setcookie("h${name}", \iMVC\Security\Hash::Generate($name.$value.'53cUr3'.'hA5h'), time()+$expire_from_now);
+    }
+    /**
+     * Delete a cookie
+     * @param string $name
+     */
+    public static function Delete($name)
+    {
+        self::Set($name, $name, -(24*3600));
+        self::Set("h${name}", $name, -(24*3600));
+    }
+    /**
+     * Check if a cookie exists or not
+     * @param string $name the cookie's name
+     * @return boolean if cookie exist and its secure
+     * @throws \iMVC\Exceptions\SecurityException if the expected cookie does not match with its hashed cookie.
+     */
+    public  static function Contains($name)
+    {
+        if(!isset($_COOKIE[$name]) || !isset($_COOKIE["h${name}"]))
+        {
+            self::Delete($name);
+            return false;
+        }
+        if(\iMVC\Security\Hash::Generate($name.$_COOKIE[$name].'53cUr3'.'hA5h')!=$_COOKIE["h${name}"])
+        {
+            self::Delete($name);
+            return false;
+            # throw new \iMVC\Exceptions\SecurityException("The cookie named \{$name\} does not matched with its hashed data!");
+        }
+        return true;
+    }
+}
+
+?>
