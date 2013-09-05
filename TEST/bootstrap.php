3f725e3ab30af83b5501cdbbdbd1611585136309
dariush <b.g.dariush@gmail.com>
2013-09-05 04:47:20 +0430

    primary TEST units are responding OK

--- TEST/bootstrap.php
+++ TEST/bootstrap.php
@@ -1,21 +1,4 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * @author dariush
- */
-// TODO: check include path
-ini_set('include_path', implode(PATH_SEPARATOR, array(ini_get('include_path'),  realpath(dirname(__FILE__)."/../"))));
-
-chdir(realpath(dirname(__FILE__)));
-
-spl_autoload_register(function ($class) {
-        if(strpos($class, "iMVC")===false) return;
-        $r = explode("\\", $class);
-        unset($r[0]);
-        $c = implode(DIRECTORY_SEPARATOR, $r);
-        require_once "../$c.php";
-});
+// this will do essential prepartion for iMVC env
+require_once dirname(__FILE__).'/../baseiMVC.php';
