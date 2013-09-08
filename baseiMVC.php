f05d6323f97b47cbd11048640761616bcfda3b4b
dariush <b.g.dariush@gmail.com>
2013-09-08 08:38:51 +0430

    TEST files for iMVC generated

--- baseiMVC.php
+++ baseiMVC.php
@@ -4,13 +4,12 @@ namespace iMVC;
 if(!defined("IMVC_ROOT") || !defined('PROJECT_ROOT'))
 {
     # define iMVC ROOT
-    define("IMVC_ROOT", dirname(__FILE__)."/");
+    defined('IMVC_ROOT') || define("IMVC_ROOT", dirname(__FILE__)."/");
     # dfine project ROOT
     defined("PROJECT_ROOT") || define("PROJECT_ROOT", dirname(IMVC_ROOT).DIRECTORY_SEPARATOR);
     # set include path to project root
     # every class' namespace should be a map from project root
     ini_set('include_path', implode(PATH_SEPARATOR, array(ini_get('include_path'),  PROJECT_ROOT)));
-    
     spl_autoload_register(
         function ($class) {
             # fetch relative path using namespace map
@@ -31,29 +30,21 @@ if(!defined("IMVC_ROOT") || !defined('PROJECT_ROOT'))
  */
 abstract class baseiMVC extends \stdClass
 {
-	function __construct()
-	{
-	}
+    /**
+     * This will dispose any temp attributes which added by __set()
+     */
+    public function Dispose()
+    {
+        foreach($this as $key => $value)
+        {
+            unset($this->$key);
+            unset($value);
+        }
+    }
 
-	function __destruct()
-	{
-	}
-
-	/**
-	 * This will dispose any temp attributes which added by __set()
-	 */
-	public function Dispose()
-	{
-            foreach($this as $key => $value)
-            {
-                unset($this->$key);
-                unset($value);
-            }
-	}
-
-	/**
-	 * The initiation works on loading class
-	 */
-	abstract public function Initiate();
+    /**
+     * The initiation works on loading class
+     */
+    abstract public function Initiate();
 }
 ?>
