b5623ee542d0f09e706a913a072f02596a1ad481
dariush <b.g.dariush@gmail.com>
2013-09-17 20:10:02 +0430

    Router boostrapper handler in effect

--- kernel/routing/router.php
+++ kernel/routing/router.php
@@ -1,7 +1,6 @@
 <?php
 namespace zinux\kernel\routing;
 
-require_once ('request.php');
 require_once (dirname(__FILE__).'/../../baseZinux.php');
 
 
@@ -12,50 +11,59 @@ require_once (dirname(__FILE__).'/../../baseZinux.php');
  */
 class router extends \zinux\baseZinux
 {
-    protected  static $_boostraps;
+    protected  static $_boostraps = array();
     
     public function __construct()
     {
         $this->Initiate();
     }
     
-    public function Initiate()
+    public function Initiate(){}
+    
+    public function Process(request &$request)
     {
-        self::$_boostraps = array();
+        $this->RunBootstrap($request);
+        $request->Process();
+        $this->request = $request;
     }
     /**
      * Route the passed request
      * 
      * @param request
      */
-    public function Run(request &$request)
-    {
-        $this->RunBootstrap($request);
-        
-        $request->Process();
+    public function Run()
+    {        
+        $this->request->Process();
         // pre-dispatcher
         $predisp = new preDispatcher();
-        $predisp->Process($request);
+        $predisp->Process($this->request);
         // dispatcher
         $disp = new dispatcher();
-        $disp->Process($request);
+        $disp->Process($this->request);
         // post-dispatcher
         $postdisp = new postDispatcher();
-        $postdisp->Process($request);  
-    }
-    protected function RunBootstrap(request &$request)
-    {
-        foreach(self::$_boostraps as $callback)
-        {
-            call_user_func_array($callback, array($request));
-        }
-    }
-    public static function RegisterBootstrap($callback, $name = "default")
-    {
-        self::$_boostraps[$name] = $callback;
+        $postdisp->Process($this->request);  
     }
     
-    public static function UnregisterBootstrap($name = "default")
+    protected function RunBootstrap(request &$request)
+    {
+        foreach(self::$_boostraps as $routerBootstrap)
+        {
+            $routerBootstrap->Fetch();
+            if($routerBootstrap->process($request))
+                break;
+        }
+    }
+    
+    public static function RegisterBootstrap(routerBootstrap $routerBootstrap, $name = "")
+    {
+        if(strlen($name))
+            self::$_boostraps[$name] = $routerBootstrap;
+        else
+            self::$_boostraps[] = $routerBootstrap;
+    }
+    
+    public static function UnregisterBootstrap($name)
     {
         unset(self::$_boostraps[$name]);
     }
