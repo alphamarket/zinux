9cefb045b65b251be76063732e4632af2bdd4a41
dariush <b.g.dariush@gmail.com>
2013-09-17 20:12:52 +0430

    router boostrap is in effect

---
+++ kernel/routing/routerBootstrap.php
@@ -0,0 +1,48 @@
+<?php
+namespace zinux\kernel\routing;
+
+require_once (dirname(__FILE__).'/../../baseZinux.php');
+
+abstract class routerBootstrap extends \zinux\baseZinux
+{
+    private $routes = array();
+    
+    public abstract function Fetch();
+    
+    public function Initiate()
+    {
+        $this->routes = array();
+    }
+    
+    public function __construct()
+    {
+        $this->Initiate();
+    }
+    
+    private function regexPath($path)
+    {
+        return '#' . preg_replace(array("/:int:/i", "/:string:/i", "/[$]\d+/i", "#^/#", "#/$#"), array("(\d+)", "(.+)", "(.+)", "", ""), $path) . '#i';
+    }
+
+    public function addRoute($pattern, $target)
+    {
+        $pattern = trim($pattern, "/");
+        $target = trim($target, "/");
+        $this->routes[$pattern] = $target;
+    }
+    
+    public function process(request &$request)
+    {
+        $uri = trim($request->GetURI(), "/");
+        foreach($this->routes as $key=> $value)
+        {
+            $pp = $this->regexPath($key);
+            if(preg_match($pp, $uri))
+            {
+                $request->SetURI("/".preg_replace($pp, $value, $uri));
+                return true;
+            }
+        }
+        return false;
+    }
+}
