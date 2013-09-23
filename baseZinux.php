9d25849bc2790934a5d603581cb42a9eafaa98fe
dariush <b.g.dariush@gmail.com>
2013-09-23 20:40:38 +0330

    03.00.14 is ready

--- baseZinux.php
+++ baseZinux.php
@@ -3,7 +3,7 @@ namespace zinux;
 
 if(!defined("ZINUX_ROOT") || !defined('PROJECT_ROOT'))
 {
-    defined("ZINUX_BUILD_VERSION") || define("ZINUX_BUILD_VERSION", "03.00.13");
+    defined("ZINUX_BUILD_VERSION") || define("ZINUX_BUILD_VERSION", "03.00.14");
     
     if(version_compare(PHP_VERSION, ZINUX_BUILD_VERSION, "<"))
     {
