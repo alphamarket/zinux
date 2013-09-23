ee00d72eb94612255dc55db98599b6deb0af838e
dariush <b.g.dariush@gmail.com>
2013-09-23 08:31:29 +0330

    + /zinux/kernel/security/encryption.php

--- baseZinux.php
+++ baseZinux.php
@@ -3,7 +3,7 @@ namespace zinux;
 
 if(!defined("ZINUX_ROOT") || !defined('PROJECT_ROOT'))
 {
-    defined("ZINUX_BUILD_VERSION") || define("ZINUX_BUILD_VERSION", "03.00.11");
+    defined("ZINUX_BUILD_VERSION") || define("ZINUX_BUILD_VERSION", "03.00.12");
     
     if(version_compare(PHP_VERSION, ZINUX_BUILD_VERSION, "<"))
     {
