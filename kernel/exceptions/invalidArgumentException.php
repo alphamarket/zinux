d839c8e0874997baa280a03de1f6010ec4f91ee5
dariush <b.g.dariush@gmail.com>
2014-05-13 21:59:47 +0430

    @ rename: kernel/exceptions/invalideOperationException.php -> kernel/exceptions/invalidOperationException.php @ rename: kernel/exceptions/invalideArgumentException.php -> kernel/exceptions/invalidArgumentException.php

--- kernel/exceptions/invalidArgumentException.php
+++ kernel/exceptions/invalidArgumentException.php
@@ -9,7 +9,7 @@ require_once ('appException.php');
  * @version 1.0
  * @created 04-Sep-2013 15:50:23
  */
-class invalideArgumentException extends appException
+class invalidArgumentException extends appException
 {
 
 	/**
--- kernel/exceptions/invalidOperationException.php
+++ kernel/exceptions/invalidOperationException.php
@@ -9,7 +9,7 @@ require_once ('appException.php');
  * @version 1.0
  * @created 04-Sep-2013 15:50:23
  */
-class invalideOperationException extends appException
+class invalidOperationException extends appException
 {
 
 	/**
